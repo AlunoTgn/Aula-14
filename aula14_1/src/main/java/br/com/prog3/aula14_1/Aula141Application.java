d6f30bff1f39bae9332ae6efba55e9419ee6ad52
Carlos Nt <carlosgabreuneto@gmail.com>
2020-07-11 15:42:38 -0300

    Mudanças no index.html

---
+++ aula14_1/src/main/java/br/com/prog3/aula14_1/Aula141Application.java
@@ -0,0 +1,13 @@
+package br.com.prog3.aula14_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula141Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula141Application.class, args);
+	}
+
+}
---
+++ aula14_1/src/test/java/br/com/prog3/aula14_1/Aula141ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prog3.aula14_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula141ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
